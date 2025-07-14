6f1ffb62748e4707bff7a9f23432449060be3b93
Miguel Wiest <wiestmiguel@gmail.com>
2025-07-14 03:50:21 -0300

    build: implements main file with global providers

---
+++ src/main.tsx
@@ -0,0 +1,32 @@
+import ReactDOM from "react-dom/client";
+import {
+    RouterProvider,
+} from "react-router";
+
+import {ThemeProvider} from "styled-components";
+import {QueryClientProvider, QueryClient} from '@tanstack/react-query'
+
+import {router} from "./routes/router.tsx";
+import {defaultTheme} from "./shared/styles/theme.ts";
+import {GlobalStyle} from "./shared/styles/global.ts";
+
+const root = document.getElementById("root") as HTMLElement;
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+            retry: 1,
+            staleTime: 1000 * 60 * 5, // 5 minutes
+        },
+    },
+});
+
+ReactDOM.createRoot(root).render(
+    <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={defaultTheme}>
+            <GlobalStyle />
+            <RouterProvider router={router} />
+        </ThemeProvider>
+    </QueryClientProvider>
+);
