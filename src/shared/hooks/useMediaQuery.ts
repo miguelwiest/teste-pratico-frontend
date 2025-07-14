3752b0ac4c1b47c1929aa8da036a7c95a8331d32
Miguel Wiest <wiestmiguel@gmail.com>
2025-07-14 20:24:23 -0300

    refact: change hook for rerendering when change media width and height

--- src/shared/hooks/useMediaQuery.ts
+++ src/shared/hooks/useMediaQuery.ts
@@ -1,14 +1,21 @@
 import {useEffect, useState} from 'react';
 
-export const useMediaQuery = (query: string) => {
-    const mediaMatch = window.matchMedia(query);
-    const [matches, setMatches] = useState(mediaMatch.matches);
+export const useMediaQuery = (query: string): boolean => {
+    const [matches, setMatches] = useState(() => window.matchMedia(query).matches);
 
     useEffect(() => {
-        const handler = (e: MediaQueryListEvent) => setMatches(e.matches);
-        mediaMatch.addListener(handler);
-        return () => mediaMatch.removeListener(handler);
-    });
+        const mediaQueryList = window.matchMedia(query);
+
+        const handler = (event: MediaQueryListEvent) => {
+            setMatches(event.matches);
+        };
+
+        mediaQueryList.addEventListener('change', handler);
+
+        return () => {
+            mediaQueryList.removeEventListener('change', handler);
+        };
+    }, [query]);
 
     return matches;
 };
