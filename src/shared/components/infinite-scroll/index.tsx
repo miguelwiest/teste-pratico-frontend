7a7834db27c142a91d135c201a3de8a0851e4a8b
Miguel Wiest <wiestmiguel@gmail.com>
2025-07-14 20:23:05 -0300

    feat: add a component for infinite scroll for get data

---
+++ src/shared/components/infinite-scroll/index.tsx
@@ -0,0 +1,64 @@
+import React, {useEffect, memo, type CSSProperties} from 'react';
+
+interface InfiniteScrollProps {
+    next: () => void;
+    hasMore: boolean;
+    loader: React.ReactNode;
+    isLoading: boolean;
+    threshold?: number;
+    scrollableTarget?: string;
+    style?: CSSProperties;
+    children: React.ReactNode;
+}
+
+const InfiniteScroll: React.FC<InfiniteScrollProps> = (
+    {
+        next,
+        hasMore,
+        loader,
+        isLoading,
+        threshold = 20,
+        scrollableTarget,
+        style,
+        children
+    }
+) => {
+    useEffect(() => {
+        const targetElement = scrollableTarget ? document.getElementById(scrollableTarget) : window;
+
+        const handleScroll = () => {
+            const scrollCheckElement = scrollableTarget
+                ? document.getElementById(scrollableTarget)
+                : document.documentElement;
+
+            if (!scrollCheckElement) {
+                return;
+            }
+
+            const {scrollTop, clientHeight, scrollHeight} = scrollCheckElement;
+
+            if (hasMore && !isLoading && (scrollTop + clientHeight >= scrollHeight - threshold)) {
+                next();
+            }
+        };
+
+        if (targetElement) {
+            targetElement.addEventListener('scroll', handleScroll);
+        }
+
+        return () => {
+            if (targetElement) {
+                targetElement.removeEventListener('scroll', handleScroll);
+            }
+        };
+    }, [next, hasMore, threshold, scrollableTarget]);
+
+    return (
+        <div style={style}>
+            {children}
+            {isLoading && loader}
+        </div>
+    );
+};
+
+export default memo(InfiniteScroll);
