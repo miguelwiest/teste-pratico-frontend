4181a1b2b611f41671be341bb80f1149fc528326
Miguel Wiest <wiestmiguel@gmail.com>
2025-07-15 04:03:29 -0300

    test: implement test for employees service methods

---
+++ src/shared/services/__test__/employees.service.test.ts
@@ -0,0 +1,64 @@
+import {getAll} from '../employees.service.ts';
+import axios from 'axios';
+import type {Employee} from "../../models/employee.model.ts";
+
+jest.mock('axios');
+
+const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation(() => {
+});
+
+describe('getAll', () => {
+
+    beforeEach(() => {
+        consoleErrorSpy.mockClear();
+    });
+
+    afterAll(() => {
+        consoleErrorSpy.mockRestore();
+    });
+
+    it('returns employees when API call is successful', async () => {
+        const mockData = [{id: 1, name: 'John Doe'}];
+        (axios.get as jest.Mock).mockResolvedValue({data: mockData});
+
+        const result = await getAll({pageParam: 1, searchTerm: ''});
+
+        expect(result).toEqual(mockData);
+    });
+
+    it('returns an empty array when API call fails', async () => {
+        (axios.get as jest.Mock).mockRejectedValue(new Error('Network Error'));
+
+        const result = await getAll({pageParam: 1, searchTerm: ''});
+
+        expect(result).toEqual([]);
+        expect(consoleErrorSpy).toHaveBeenCalledWith('Error fetching employees:', expect.any(Error));
+    });
+
+    it('constructs the correct URL with default parameters', async () => {
+        const mockData: Employee[] = [];
+        (axios.get as jest.Mock).mockResolvedValue({data: mockData});
+
+        await getAll({pageParam: 1, searchTerm: ''});
+
+        expect(axios.get).toHaveBeenCalledWith(`${import.meta.env.VITE_API_BASE_URL}/employees?_page=1&_limit=10`);
+    });
+
+    it('constructs the correct URL with a search term', async () => {
+        const mockData: Employee[] = [];
+        (axios.get as jest.Mock).mockResolvedValue({data: mockData});
+
+        await getAll({pageParam: 1, searchTerm: 'Jane'});
+
+        expect(axios.get).toHaveBeenCalledWith(`${import.meta.env.VITE_API_BASE_URL}/employees?_page=1&_limit=10&name=Jane`);
+    });
+
+    it('encodes special characters in the search term', async () => {
+        const mockData: Employee[] = [];
+        (axios.get as jest.Mock).mockResolvedValue({data: mockData});
+
+        await getAll({pageParam: 1, searchTerm: 'John Doe & Co'});
+
+        expect(axios.get).toHaveBeenCalledWith(`${import.meta.env.VITE_API_BASE_URL}/employees?_page=1&_limit=10&name=John%20Doe%20%26%20Co`);
+    });
+});
