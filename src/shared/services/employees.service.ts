0a5328b2c140c1b81322c300ec1e9af1fe1c2051
Miguel Wiest <wiestmiguel@gmail.com>
2025-07-15 04:29:30 -0300

    refact: change term in url param for better search employees

--- src/shared/services/employees.service.ts
+++ src/shared/services/employees.service.ts
@@ -8,7 +8,7 @@ export async function getAll({pageParam = 1, searchTerm = ''}): Promise<Employee
         let url = `${import.meta.env.VITE_API_BASE_URL}/employees?_page=${pageParam}&_limit=${LIMIT}`;
 
         if (searchTerm) {
-            url += `&name=${encodeURIComponent(searchTerm)}`;
+            url += `&q=${encodeURIComponent(searchTerm)}`;
         }
 
         const response = await axios.get(url);
