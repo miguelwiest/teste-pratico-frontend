d9076da6ad3b8c5534a5b2ff82bb31e74eed8db4
Miguel Wiest <wiestmiguel@gmail.com>
2025-07-15 05:09:52 -0300

    refact(service): add method for phone number and date

--- src/shared/services/employees.service.ts
+++ src/shared/services/employees.service.ts
@@ -1,14 +1,19 @@
 import type {Employee} from "../models/employee.model.ts";
+// Library Methods
 import axios from "axios";
+
+// Utils
+import {processSearchTerm} from "../../shared/utils";
 
 export async function getAll({pageParam = 1, searchTerm = ''}): Promise<Employee[]> {
     const LIMIT = 10;
     try {
+        const processedSearchTerm = processSearchTerm(searchTerm);
 
         let url = `${import.meta.env.VITE_API_BASE_URL}/employees?_page=${pageParam}&_limit=${LIMIT}`;
 
-        if (searchTerm) {
-            url += `&q=${encodeURIComponent(searchTerm)}`;
+        if (processedSearchTerm) {
+            url += `&q=${encodeURIComponent(processedSearchTerm)}`;
         }
 
         const response = await axios.get(url);
